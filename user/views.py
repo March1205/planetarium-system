91117bbb6fb2d182f0b5aa2fda5bcaadc4a7481b
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-01 12:42:07 +0200

    add user views for registration, token creation and user management

--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(TokenObtainPairView):
+    serializer_class = AuthTokenSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
