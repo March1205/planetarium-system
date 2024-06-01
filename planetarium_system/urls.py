c1d3070dd8de139bd8976336d612e993e278b090
Marharyta Marchenko <ritanika12@gmail.com>
2024-06-01 17:49:12 +0200

    fix indentation in planetarium_system/urls.py

--- planetarium_system/urls.py
+++ planetarium_system/urls.py
@@ -5,18 +5,13 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path("api/planetarium/", include(
-                      "planetarium.urls", namespace="planetarium"
-                  )),
-                  path("api/user/", include("user.urls", namespace="user")),
-                  path("api/v1/schema/", SpectacularAPIView.as_view(),
-                       name="schema"),
-                  path(
-                      "api/doc/swagger/",
-                      SpectacularSwaggerView.as_view(url_name="schema"),
-                      name="swagger-ui"
-                  )
-              ] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+    path("admin/", admin.site.urls),
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
